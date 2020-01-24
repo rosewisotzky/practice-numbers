5094821aca3ce55394053666fb6630024193faa4
Rose Wisotzky <rose@rosewisotzky.com>
2020-01-24 12:14:00 -0600

    random numbers

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace random_numbers
 {
@@ -6,7 +7,28 @@ namespace random_numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Random Numbers");
+            Random random = new Random();
+            List<int> numbers = new List<int> {
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                                                };
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                // Determine if the current loop index is contained inside of the `numbers` list. Print a message to the console indicating whether the index is in the list.
+                if(numbers.Contains(i)){
+                    Console.WriteLine($"Number {i} is in the list");
+                }
+                else {
+                    Console.WriteLine($"Number {i} is not in the list");
+                }
+                
+
+            }
         }
     }
 }
